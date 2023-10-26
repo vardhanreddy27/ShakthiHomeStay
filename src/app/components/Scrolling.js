88d5d5373acb9c7033c88b0bc17c77a6860c428f
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-10-27 01:16:58 +0530

    fixed almos doneas

--- src/app/components/Scrolling.js
+++ src/app/components/Scrolling.js
@@ -6,7 +6,7 @@ function Scrolling() {
   return (
     <div className='container mt-5 back'>
       <div className='row text-center onlymob'><h3>We are also Available In </h3></div>
-<div className='row mt-3 pb-5 onlyinmob'>
+<div className='row mt-3 onlyinmob'>
 <Marquee autoFill="true">
 <div className='col iconscroll'>  <Image 
         src="/my.png"
