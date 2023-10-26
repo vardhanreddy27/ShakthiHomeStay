88e0bb657f7d722de7c6ffc5d8d493076441b4f7
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-10-27 00:23:48 +0530

    fixed almos done

--- src/app/components/Scrolling.js
+++ src/app/components/Scrolling.js
@@ -5,8 +5,8 @@ import Marquee from "react-fast-marquee";
 function Scrolling() {
   return (
     <div className='container mt-5 back'>
-      <div className='row text-center'><h3>We are also Available In </h3></div>
-<div className='row mt-3 pb-5'>
+      <div className='row text-center onlymob'><h3>We are also Available In </h3></div>
+<div className='row mt-3 pb-5 onlyinmob'>
 <Marquee autoFill="true">
 <div className='col iconscroll'>  <Image 
         src="/my.png"
