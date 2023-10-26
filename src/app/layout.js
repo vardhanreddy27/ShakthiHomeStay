d6762ab0e25c2bffd18a2b77c20bbdc16396906d
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-10-27 01:02:43 +0530

    fixed almos donesa

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en"><head>
-                <link rel="icon" href="/favicon1.ico" />
+                <link rel="shortcut icon" href="/public/favicon.png" />
        <Script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></Script>
        <link rel="preconnect" href="https://fonts.googleapis.com" />
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
 <Script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></Script> <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></Script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossOrigin="anonymous"></script>
-  </head>
+    </head>
       <body className={inter.className}>{children}
       <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
   <script>
