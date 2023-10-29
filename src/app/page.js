a0d6cd58e921279fab8469d074d3a5557db929f3
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-10-29 19:22:34 +0530

    fixed almos doneasssaaasssllggslll

--- src/app/page.js
+++ src/app/page.js
@@ -11,21 +11,24 @@ import Pics from './components/Pics';
 import Contact from './components/Contact';
 import AnimatedCursor from "react-animated-cursor"
 import Hero from './components/Hero';
+import List from './components/List';
 import Modern from './components/Modern';
 
 function page() {
 	   return (
     <>
-    {/* <AnimatedCursor  innerSize={10}
+    <AnimatedCursor  innerSize={10}
       outerSize={50} outerAlpha={0.3}
       innerScale={0.7}
       color='3,74,74'
-      outerScale={2} /> */}
+      outerScale={2} />
  <Navbar />
 <HeroBanner />
 <Features />
 <Hero />
 <Modern />
+<List />
+
 <Data />
 <Scrolling />
 <Services /> 
