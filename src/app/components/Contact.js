cc09d488acdc197d4518816484ae924106206e9b
Reddy Palagiri <v.a.reddy.palagiri@accenture.com>
2023-11-18 16:21:51 +0530

    first commi mapt

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -7,8 +7,8 @@ function Contact() {
     <div className='fluid-container'>
       <div className='row'>
       <div className='col-md-6 rempad'>
-      <iframe className='map' src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3881.986347043406!2d75.76256907508309!3d13.351124987000212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTPCsDIxJzA0LjEiTiA3NcKwNDUnNTQuNSJF!5e0!3m2!1sen!2sin!4v1697320137537!5m2!1sen!2sin" allowFullScreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
+      <iframe src="https://www.google.com/maps/d/embed?mid=1HMGODrD2qemw8DUHL0930LQ2suaGr4Y&ehbc=2E312F"style={{width:'100%', height:'100%'}}></iframe>
+     </div>
       <div className='col-md-6 contact text-center'>
       <div className='row'>
        <a href="tel:7483505691" > <div className='col-6 colone'><FiPhoneCall className='tops' size={42}/><h5 className='p-3'>Call us 7483505691</h5></div></a>
